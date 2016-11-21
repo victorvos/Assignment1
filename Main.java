c153bd4b82a5274972ca190b03ddccd3956a93a3
Victor <me@victorvos.com>
2016-11-21 21:12:23 +0100

    werjebd net method call invoke

--- Main.java
+++ Main.java
@@ -15,7 +15,9 @@ public class Main {
     public static void main(String[] args) {
 
         ArrayList<Format> formats = new ArrayList();
-        Format f = new Format();
+        SaveAs save = new SaveAs();
+        System.out.println("Save As:");
+
 
 //        formats.add(new Format("Word'97", ".docx",1));
 //        formats.add(new Format("Word(XML)", ".doc",2));
@@ -33,18 +35,37 @@ public class Main {
                 z = m[i].toString();
                 String[] x = z.split(" ");
                 String methodName = x[2];
-                System.out.println(methodName);
+                String method1 = methodName.split("\\.")[1];
+                System.out.println(i + " " + method1);
             }
         } catch (Throwable e) {
             System.err.println(e);
         }
-//
-//
-//
 
-//        Scanner scanner = new Scanner(System.in);
-//        out.println("Kies uw index:");
-//        int index = new Integer(scanner.next());
+        Scanner scanner = new Scanner(System.in);
+        out.println("Kies uw index:");
+        int index = new Integer(scanner.next());
+
+
+        try {
+            Class c = SaveAs.class;
+            Method[] m = c.getDeclaredMethods();
+            String z = null;
+
+            z = m[index].toString();
+            String[] x = z.split(" ");
+            String methodName = x[2];
+            String method1 = methodName.split("\\.")[1];
+            Method methode = SaveAs.class.getDeclaredMethod(method1.substring(0, method1.length() - 2));
+            System.out.println(methode.invoke(save));
+
+        } catch (Throwable e) {
+            System.err.println(e);
+        }
+
+//
+//
+//
 
 //        for(Format f : formats) {
 //            if(index == f.getIndex()) {
